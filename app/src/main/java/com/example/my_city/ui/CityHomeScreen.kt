13ef0f3ecb96130fb6abc9d1cd257fced608ddb9
Nuray <210103331@stu.sdu.edu.kz>
2023-11-23 16:57:42 +0600

    3

--- app/src/main/java/com/example/my_city/ui/CityHomeScreen.kt
+++ app/src/main/java/com/example/my_city/ui/CityHomeScreen.kt
@@ -2,12 +2,17 @@ package com.example.my_city.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+
+
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,8 +22,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.my_city.R
+import com.example.my_city.data.CityData
 import com.example.my_city.model.ToDo
+import com.example.my_city.ui.theme.My_cITYTheme
 
 @Composable
 fun CityApp(){
@@ -52,11 +62,22 @@ private fun ToDoListItem(
             ToDoListImageItem(
                 toDo = toDo,
                 modifier = Modifier
+                    .size(84.dp)
+
+            )
+            Text(
+                text = stringResource(toDo.titleResourceId),
+
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .padding(20.dp)
+
 
             )
 
 
         }
+
     }
 }
 
@@ -73,4 +94,12 @@ private fun ToDoListImageItem(toDo: ToDo,modifier:Modifier = Modifier){
                 )
                 
             }
+}
+@Preview
+@Composable
+fun ToDoListItemPreview(){
+    My_cITYTheme {
+        ToDoListItem(toDo = CityData.defaultCity)
+    }
+    
 }
